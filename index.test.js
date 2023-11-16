e7fa6beeed6ed9596ec831b34b4736ee1f413e36
Zachary Honey <zhoney466@gmail.com>
2023-11-16 08:55:39 -0800

    wip

--- index.test.js
+++ index.test.js
@@ -42,9 +42,15 @@ describe('[Exercise 4] Counter', () => {
   beforeEach(() => {
     counter = new utils.Counter(3) // each test must start with a fresh couter
   })
-  // test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {})
-  // test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {})
-  // test('[8] the count eventually reaches zero but does not go below zero', () => {})
+  test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {
+    
+  })
+  test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {
+    
+  })
+  test('[8] the count eventually reaches zero but does not go below zero', () => {
+    
+  })
 })
 
 describe('[Exercise 5] Seasons', () => {
